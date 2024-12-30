be5dcf207ac4cf3fe1225fd8d2ca71888090fca8
hoanggiang2912 <giang113.hg@gmail.com>
2024-12-30 15:46:02 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <nav className="p-4">
+        <Link to="/" className="mr-4">
+          Home
+        </Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1 className="text-2xl">Home Page</h1>;
+};
+
+export default Home;
