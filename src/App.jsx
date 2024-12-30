4bfbc11b2255d6f0f8ae7eee16e32496f5dee511
thanhnq <quocthanhn87@gmail.com>
2024-12-30 16:36:19 +0700

    commit to pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
